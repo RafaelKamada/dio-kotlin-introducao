ec8fe7b1c5483861dbfebc4ad9c501d38304c6bb
RKamada <rafael-kamada2007@hotmail.com>
2022-07-23 17:39:00 -0300

    Created analyst, employee and manager class

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digioebank/Gerente.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digioebank
+
+class Gerente(
+    nome: String,
+    cpf: String,
+    salario: Double,
+    val senha: String
+): Funcionario(nome, cpf, salario), Logavel {
+    override fun calculoAuxilio(): Double = salario * 0.4
+    override fun login(): Boolean = "senha123" == senha
+}
