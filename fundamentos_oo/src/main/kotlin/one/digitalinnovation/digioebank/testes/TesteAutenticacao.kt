c1a3bed29032966a7a2cc98c214f3ed209350349
RKamada <rafael-kamada2007@hotmail.com>
2022-07-23 17:39:51 -0300

    Created login class and tests for it

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digioebank/testes/TesteAutenticacao.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digioebank.testes
+
+import one.digitalinnovation.digioebank.Logavel
+
+class TesteAutenticacao {
+    fun autentica(logavel: Logavel) = println(logavel.login())
+}
